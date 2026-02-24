2a82886163fbd165ffbe0df18ff4b1a9721c7b6e
Giritharan Janakiraman <92202831+GiritharanJ@users.noreply.github.com>
2026-02-25 00:46:43 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const menuBtn = document.getElementById("menu-btn");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+    menuBtn.addEventListener("click", function () {
+        mobileMenu.classList.toggle("hidden");
+    });
+
+});
+
 function sendToWhatsApp(service){
     window.open(
         "https://wa.me/+917708007222?text=" +
