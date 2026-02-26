5d11c45c35a9e866b2773fd5530411239629e550
Giritharan Janakiraman <92202831+GiritharanJ@users.noreply.github.com>
2026-02-26 17:23:15 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+let slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+setInterval(() => {
+    slides[currentSlide].classList.remove("opacity-100");
+    slides[currentSlide].classList.add("opacity-0");
+
+    currentSlide = (currentSlide + 1) % slides.length;
+
+    slides[currentSlide].classList.remove("opacity-0");
+    slides[currentSlide].classList.add("opacity-100");
+}, 2000);
+
 function sendToWhatsApp(service){
     window.open(
         "https://wa.me/+917708007222?text=" +
